3d2e8bc21868d4363cd7c81d44b497bfd66590a4
Arya A <aryaacikgoz@outlook.com>
2023-04-05 19:14:00 +0300

    interval and countdown logic

--- src/assets/SessionCard.tsx
+++ src/assets/SessionCard.tsx
@@ -1,25 +1,35 @@
 import {useState, useEffect} from "react";
 
 const SessionCard = (): JSX.Element => {
-    const [counterInterval, setCounterInterval] = useState({countLength: 25, breakLength: 5, seconds: 0, timerStopped: false});
+    const [counterInterval, setCounterInterval] = useState({countLength: 25, seconds: 1500, timer: ""});
 
     const handleInterval = (): void => {
-        const timer: number = counterInterval.countLength * 60;
-        setCounterInterval({...counterInterval, seconds: timer});
-        console.log(Math.floor(timer / 60)+':'+('0'+Math.floor(timer % 60)).slice(-2));
-
-// https://stackoverflow.com/questions/3733227/javascript-seconds-to-minutes-and-seconds
+        // Convert the current time in minutes to seconds
+        // Then format it like MM:SS
+        const formattedVal: string = Math.floor(counterInterval.seconds / 60) + ':' + ('0' + Math.floor(counterInterval.seconds % 60)).slice(-2);
+        // it's a state now
+        setCounterInterval({...counterInterval, timer: formattedVal});
     }
 
     useEffect(() => {
         handleInterval();
+        const goInterval = setInterval(() => {
+            if (counterInterval.seconds > 0) {
+                setCounterInterval({...counterInterval, seconds: (counterInterval.seconds - 1)});
+            } else {
+                clearInterval(goInterval);
+            }
 
-        }, [])
+        }, 1000)
+        return (): void=> {
+            clearInterval(goInterval);
+        };
+        }, [counterInterval.seconds])
 
 
     return <div className="border-2 border-white rounded-3xl p-5 text-center">
         <h3 className="capitalize text-2xl font-medium">session</h3>
-        <span className="text-6xl">{counterInterval.seconds}</span>
+        <span className="text-6xl">{counterInterval.timer}</span>
     </div>
 }
 
